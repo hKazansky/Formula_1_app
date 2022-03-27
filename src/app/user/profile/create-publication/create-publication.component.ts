5f686bc3348fe703ca7d189db0f74792f9c742bb
hKazansky <hrkazanski@gmail.com>
2022-03-27 17:32:19 +0300

    Added create post functionality
    Implemented the functionality of creating post. The data is stored in the database.

--- src/app/user/profile/create-publication/create-publication.component.ts
+++ src/app/user/profile/create-publication/create-publication.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostService } from './../../../services/post.service'
 @Component({
   selector: 'app-create-publication',
   templateUrl: './create-publication.component.html',
   styleUrls: ['./create-publication.component.css']
 })
-export class CreatePublicationComponent implements OnInit {
+export class CreatePublicationComponent {
 
-  constructor() { }
+  constructor(private router: Router, private service: PostService) { }
 
-  ngOnInit(): void {
+  form = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    description: new FormControl('', [Validators.required, Validators.minLength(20)])
+  })
+
+  onSubmitCreatePost() {
+    this.service.createPost(this.form.value).subscribe()
+    this.router.navigate(['/'])
+
   }
 
 }
