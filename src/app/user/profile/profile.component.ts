63f70965416aa8bc542cf1085a71e7ae1eb26ce1
hKazansky <hrkazanski@gmail.com>
2022-03-27 15:40:20 +0300

    Edit Profile component
    Deleted some code that I was texting a form with.

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,27 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { PostService } from 'src/app/services/post.service';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
 
-  constructor(private postService: PostService) { }
-
-  ngOnInit(): void {
-    this.onSubmitCreatePost()
-  }
-  
-  form = new FormGroup({
-    'title': new FormControl('', [Validators.required]),
-    'description': new FormControl('', [Validators.required]),
-   
-  })
-  
-  onSubmitCreatePost() {
-    this.postService.createPost(this.form.value).subscribe(data => console.log(data))
-  }
+  constructor() { }
 
 }
