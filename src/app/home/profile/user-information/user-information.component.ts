50de081af640f937c6b3561ddfc97e14262d59f2
hKazansky <hrkazanski@gmail.com>
2022-03-28 08:56:27 +0300

    get the current user from db

--- src/app/home/profile/user-information/user-information.component.ts
+++ src/app/home/profile/user-information/user-information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-information',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-information.component.css']
 })
 export class UserInformationComponent implements OnInit {
+  allUsersFromDatabase: any[] = [];
+  currentUser: any;
 
-  constructor() { }
+  constructor(private service: UserService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    this.service.getAllUsers().subscribe(data => {
+      this.allUsersFromDatabase = data;
+      this.currentUser = this.allUsersFromDatabase.filter((user) => user.email === localStorage.getItem('email'))[0];
+    })
   }
 
 }
