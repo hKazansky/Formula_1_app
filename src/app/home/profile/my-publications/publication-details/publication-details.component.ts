d985644eedd4c227f3e8db79d6fa77f53a828c86
hKazansky <hrkazanski@gmail.com>
2022-03-29 08:33:04 +0300

    Implementing reactive form to the Comments section

--- src/app/home/profile/my-publications/publication-details/publication-details.component.ts
+++ src/app/home/profile/my-publications/publication-details/publication-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from 'src/app/services/post.service';
-
+import { FormGroup, FormControl, Validators, NgModel } from '@angular/forms';
 @Component({
   selector: 'app-publication-details',
   templateUrl: './publication-details.component.html',
@@ -12,19 +12,37 @@ export class PublicationDetailsComponent implements OnInit {
   currentPost: any;
   postId: string = '';
 
+
   constructor(private service: PostService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getCurrentPost();
+
   }
+
+  form = new FormGroup({
+    comment: new FormControl('', [Validators.required]),
+  });
 
   getCurrentPost() {
     this.postId = Object.values(this.route.snapshot.params)[0];
     this.service.getAllPublications().subscribe(data => {
       this.allPosts = data;
       this.currentPost = this.allPosts.filter(post => post._id === this.postId)[0];
-      console.log(this.currentPost)
     })
   }
 
+  postComment() {
+    this.postId = Object.values(this.route.snapshot.params)[0];
+
+    this.service.createComment(this.form.value, this.postId).subscribe();
+
+    let inputField = (<HTMLInputElement>document.getElementById('comment-input'))
+    inputField.value = '';
+  }
+
+  toggleComments() {
+    const comments = this.service.getPublicationComments().subscribe(data => console.log(data));
+    
+  }
 }
