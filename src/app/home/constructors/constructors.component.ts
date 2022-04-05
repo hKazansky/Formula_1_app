f8bd3b93100da69203c2bc72bfba3398313b6492
hKazansky <hrkazanski@gmail.com>
2022-04-05 12:39:09 +0300

    Added animations to the constructors page

--- src/app/home/constructors/constructors.component.ts
+++ src/app/home/constructors/constructors.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { fade, transitDates } from 'src/app/animations';
 import { IConstructor } from 'src/app/interfaces/constructor';
 import { ConstructorsService } from 'src/app/services/constructors.service';
 
 @Component({
   selector: 'app-constructors',
   templateUrl: './constructors.component.html',
-  styleUrls: ['./constructors.component.css']
+  styleUrls: ['./constructors.component.css'],
+  animations: [transitDates, fade]
 })
 export class ConstructorsComponent implements OnInit {
   constructors: any
@@ -19,7 +21,7 @@ export class ConstructorsComponent implements OnInit {
   loadConstructorsData(): void {
     this.constructorsService.loadConstructors().subscribe(c => {
       // this.constructors = c.MRData.StandingsTable.StandingsLists);
-     this.constructors = c[0].StandingsLists[0].ConstructorStandings
+      this.constructors = c[0].StandingsLists[0].ConstructorStandings
     }
     )
   }
