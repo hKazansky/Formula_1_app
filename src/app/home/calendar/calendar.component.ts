ced00afc36a065b4df1c5f507f898d210636d03b
hKazansky <hrkazanski@gmail.com>
2022-04-05 12:38:47 +0300

    Animation added to the calendar component

--- src/app/home/calendar/calendar.component.ts
+++ src/app/home/calendar/calendar.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { fade } from 'src/app/animations';
 import { IRace } from 'src/app/interfaces/race';
 import { CalendarService } from 'src/app/services/calendar.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
+  animations: [
+   fade  
+  ]
 })
 export class CalendarComponent implements OnInit {
   calendar!: IRace[]
