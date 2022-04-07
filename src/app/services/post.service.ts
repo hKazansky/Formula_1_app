c7fc8b8c5598f055da2bff2d6d8fa55a57d69a63
hKazansky <hrkazanski@gmail.com>
2022-04-07 19:50:51 +0300

    Added error handling

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,26 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPosts } from '../interfaces/posts';
 import { IComments } from '../interfaces/comments';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
   constructor(private http: HttpClient) { }
-
-  createPost(body: any) {
-    return this.http.post<any>('http://localhost:3000/create', body)
-  }
-
-  createComment(body: any, postId: any) {
-    const params = new HttpParams({
-      fromObject: {
-        postId
-      }
-    });
-
-    return this.http.post<any>('http://localhost:3000/create-comment', body, { params: params })
-  }
 
   getAllPublications() {
     return this.http.get<IPosts[]>('http://localhost:3000/posts');
@@ -41,14 +30,18 @@ export class PostService {
     return this.http.get<IPosts>(`http://localhost:3000/posts/${postId}`, { params: params })
   }
 
-  editPost(postId: string, body: any) {
+  createPost(body: any): Observable<any> {
+    return this.http.post<any>('http://localhost:3000/create', body).pipe(catchError(this.errorHandler))
+  }
+
+  editPost(postId: string, body: any): Observable<any> {
     const params = new HttpParams({
       fromObject: {
         postId
       }
     });
 
-    return this.http.put<any>(`http://localhost:3000/posts/${postId}/edit`, body, { params: params })
+    return this.http.put<any>(`http://localhost:3000/posts/${postId}/edit`, body, { params: params }).pipe(catchError(this.errorHandler))
   }
 
   deletePost(postId: string) {
@@ -60,6 +53,17 @@ export class PostService {
 
     return this.http.delete<any>(`http://localhost:3000/posts/${postId}/delete`, { params: params })
   }
+
+  createComment(body: any, postId: any) {
+    const params = new HttpParams({
+      fromObject: {
+        postId
+      }
+    });
+
+    return this.http.post<any>('http://localhost:3000/create-comment', body, { params: params })
+  }
+
 
   deleteComment(commentId: any, postId: any) {
     const params = new HttpParams({
@@ -90,6 +94,10 @@ export class PostService {
     });
 
     return this.http.post<any>(`http://localhost:3000/comments/${commentId}/dislike`, '', { params: params })
+  }
+
+  errorHandler(error: any) {
+    return throwError(error || 'Server error')
   }
 
 }
