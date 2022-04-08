1ee1cc64d25d98beb246a026a750fa15b60bda5e
hKazansky <hrkazanski@gmail.com>
2022-04-08 12:29:01 +0300

    Added 2 more user service methods

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IUser } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,14 @@ export class UserService {
 
   getAllUsers() {
     return this.http.get<any>('http://localhost:3000/users')
+  }
+
+  getUserById() {
+    return this.http.get<IUser>(`http://localhost:3000/user/:userId`)
+  }
+
+  editUserInformation(userData: object) {
+    return this.http.put<IUser>(`http://localhost:3000/user/:userId/edit`, userData)
   }
 
 }
