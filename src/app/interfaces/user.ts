5f2d0547570d6b98165238f64c92198572be869f
hKazansky <hrkazanski@gmail.com>
2022-04-08 12:27:19 +0300

    Added user interface

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    birthday: string
+    country: string
+    email: string
+    fullName: string
+    password: string
+    team: string
+    __v: number
+    _id: string
+}
