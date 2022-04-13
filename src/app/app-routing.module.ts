3be3926f498561cf8bbc7e546452ed8bbbdad97f
hKazansky <hrkazanski@gmail.com>
2022-04-13 11:36:12 +0300

    Added canLoad guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { NonAuthGuard } from './guards/non-auth.guard';
 import { AboutComponent } from './home/about/about.component';
 import { WildcardComponent } from './home/wildcard/wildcard.component';
 
@@ -12,31 +14,43 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('src/app/home/main-view/main-view.module').then(m => m.MainViewModule)
+    loadChildren: () => import('src/app/home/main-view/main-view.module').then(m => m.MainViewModule),
   },
   {
     path: 'calendar',
-    loadChildren: () => import('src/app/home/calendar-view/calendar-view.module').then(m => m.CalendarViewModule)
+    loadChildren: () => import('src/app/home/calendar-view/calendar-view.module').then(m => m.CalendarViewModule),
+    canLoad: [AuthGuard]
+
   },
   {
     path: 'drivers',
-    loadChildren: () => import('src/app/home/drivers-view/drivers-view.module').then(m => m.DriversViewModule)
+    loadChildren: () => import('src/app/home/drivers-view/drivers-view.module').then(m => m.DriversViewModule),
+    canLoad: [AuthGuard]
+
   },
   {
     path: 'constructors',
-    loadChildren: () => import('src/app/home/constructors-view/constructors-view.module').then(m => m.ConstructorsViewModule)
+    loadChildren: () => import('src/app/home/constructors-view/constructors-view.module').then(m => m.ConstructorsViewModule),
+    canLoad: [AuthGuard]
+
   },
   {
     path: 'profile',
-    loadChildren: () => import('src/app/home/profile-view/profile-view.module').then(m => m.ProfileViewModule)
+    loadChildren: () => import('src/app/home/profile-view/profile-view.module').then(m => m.ProfileViewModule),
+    canLoad: [AuthGuard]
+
   },
   {
     path: 'forum',
-    loadChildren: () => import('src/app/home/forum-view/forum-view.module').then(m => m.ForumViewModule)
+    loadChildren: () => import('src/app/home/forum-view/forum-view.module').then(m => m.ForumViewModule),
+    canLoad: [AuthGuard]
+
   },
   {
     path: 'auth',
-    loadChildren: () => import('src/app/home/auth-view/auth-view.module').then(m => m.AuthViewModule)
+    loadChildren: () => import('src/app/home/auth-view/auth-view.module').then(m => m.AuthViewModule),
+    canLoad: [NonAuthGuard]
+
   },
   {
     path: 'about',
@@ -50,7 +64,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
