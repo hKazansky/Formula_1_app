457de2f421a8f7d9fe238b75b1deff681bc3fca9
hKazansky <hrkazanski@gmail.com>
2022-04-13 19:01:03 +0300

    Added drivers service

---
+++ src/app/home/drivers-view/services/drivers.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core';
+import { IDrivers } from 'src/app/interfaces/drivers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriversService {
+
+  constructor(private http: HttpClient) { };
+
+  loadDrivers() {
+    return this.http.get<IDrivers[]>(`http://localhost:3000/drivers`)
+
+  }
+
+  getDriverById(driverId: string) {
+    return this.http.get<IDrivers>(`https://ergast.com/api/f1/drivers/${driverId}.json`);
+
+  }
+}
