bb9728362e6141d8256118e0717606bf9f07faba
hKazansky <hrkazanski@gmail.com>
2022-04-13 11:37:04 +0300

    Deleted the canActivate guards

--- src/app/home/auth-view/auth-view-routing.module.ts
+++ src/app/home/auth-view/auth-view-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { NonAuthGuard } from "src/app/guards/non-auth.guard";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 
@@ -9,12 +8,10 @@ const root: Routes = [
     {
         path: 'login',
         component: LoginComponent,
-        canActivate: [NonAuthGuard]
     },
     {
         path: 'register',
         component: RegisterComponent,
-        canActivate: [NonAuthGuard]
     }
 
 ]
