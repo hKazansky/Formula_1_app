5b386172030476024b9eeee1308a4886ef93da97
hKazansky <hrkazanski@gmail.com>
2022-04-13 11:38:15 +0300

    Added comments service
    Transfer the logic from the post service to a separate service

---
+++ src/app/services/comments.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IComments } from '../interfaces/comments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentsService {
+
+  constructor(private http: HttpClient) { }
+
+  getPublicationComments() {
+    return this.http.get<IComments[]>('http://localhost:3000/comments')
+  }
+
+  createComment(body: any, postId: any) {
+    const params = new HttpParams({
+      fromObject: {
+        postId
+      }
+    });
+
+    return this.http.post<any>('http://localhost:3000/create-comment', body, { params: params })
+  }
+
+  deleteComment(commentId: any, postId: any) {
+    const params = new HttpParams({
+      fromObject: {
+        commentId,
+        postId
+      }
+    });
+
+    return this.http.delete<any>(`http://localhost:3000/delete/${commentId}`, { params: params })
+  }
+
+  likeComment(commentId: string) {
+    const params = new HttpParams({
+      fromObject: {
+        commentId
+      }
+    });
+
+    return this.http.post<any>(`http://localhost:3000/comments/${commentId}/like`, '', { params: params })
+  }
+
+  dislikeComment(commentId: string) {
+    const params = new HttpParams({
+      fromObject: {
+        commentId
+      }
+    });
+
+    return this.http.post<any>(`http://localhost:3000/comments/${commentId}/dislike`, '', { params: params })
+  }
+}
