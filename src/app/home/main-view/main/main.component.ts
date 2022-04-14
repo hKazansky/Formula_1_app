bdd9a13a0b82b3b2c343df4ef846cf24da1f86ad
hKazansky <hrkazanski@gmail.com>
2022-04-14 18:51:43 +0300

    Added new function for the countDownClock

--- src/app/home/main-view/main/main.component.ts
+++ src/app/home/main-view/main/main.component.ts
@@ -4,8 +4,8 @@ import { fade, transitDates, transitTimes, transitTitles } from 'src/app/animati
 import { IRace } from 'src/app/interfaces/race';
 import { CalendarService } from 'src/app/services/calendar.service';
 import { DateTimeFormatterService } from 'src/app/services/date-time-formatter.service';
+import { RacePropertiesOverriderService } from 'src/app/services/race-properties-overrider.service';
 import { GoogleMapsService } from '../services/google-maps.service';
-import { RacePropertiesOverriderService } from '../services/race-properties-overrider.service';
 
 @Component({
   selector: 'app-main',
@@ -41,6 +41,8 @@ export class MainComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.loadNextRace();
+    this.countdownTimer();
+
   }
 
   loadNextRace(): void {
@@ -64,27 +66,30 @@ export class MainComponent implements OnInit, OnDestroy {
       this.day = this.upcomingRaceDate.slice(6);
 
       this.yearDayMonthOfUpcomingRace = `${this.year.join('')}/${this.month.join('')}/${this.day.join('')}`
+
       this.upcomingRace = this.calendar?.filter(race => race.date.split('-').join('/') === this.yearDayMonthOfUpcomingRace)[0];
 
-      this.upcomingRace.ThirdPractice ? this.sprintOrPractice = 'PRACTICE 3' : this.sprintOrPractice = 'SPRINT'
+      this.upcomingRace.ThirdPractice ? this.sprintOrPractice = 'PRACTICE 3' : this.sprintOrPractice = 'SPRINT';
 
       this.racePropertiesOverrider.racePropertiesOverrider(this.upcomingRace);
 
       this.googleMapsService.googleMapsInit(this.upcomingRace);
-
-      setInterval(() => {
-        let countDownRaceDate = this.dateTimeFormatter.getUpcomingRaceDateFormatter(this.upcomingRace);
-
-        let dateNow = new Date().getTime();
-        let distance = countDownRaceDate - dateNow;
-
-        this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      }, 1000);
     })
+  }
+
+  countdownTimer(): void {
+    setInterval(() => {
+      let countDownRaceDate = this.dateTimeFormatter.getUpcomingRaceDateFormatter(this.upcomingRace);
+
+      let dateNow = new Date().getTime();
+      let distance = countDownRaceDate - dateNow;
+
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    }, 1000);
   }
 
   ngOnDestroy(): void {
